f11742d7806de62bf1e54c2be47f63c16a702704
safinhr <hrsafin2434@gmail.com>
2024-01-05 15:13:47 +0600

    recieving input and print output

---
+++ returingInput.py
@@ -0,0 +1,10 @@
+#recieving input using input() function
+#printing output using output() function
+
+
+name = input("What is your name? ")
+age = input("What is your age? ")
+dream = input("What is your dream study country? ")
+
+
+print(name + " " +  age + " " + dream)
