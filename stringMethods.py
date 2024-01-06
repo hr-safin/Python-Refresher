34eb72a6770380b3f38c5d4a5468aa2ff4c36e11
safinhr <hrsafin2434@gmail.com>
2024-01-06 12:28:56 +0600

    revised some string methods

---
+++ stringMethods.py
@@ -0,0 +1,11 @@
+#string method
+
+name = "hasibur rahman safin"
+
+print(name.upper())
+print(len(name))
+print(name.lower())
+print(name.find("h"))
+print(name.replace("safin", "programmer safin"))
+print(name.capitalize())
+print(name.title())
