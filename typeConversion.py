994db01ff7a729dba60c9ed5f788f9859405ada7
safinhr <hrsafin2434@gmail.com>
2024-01-06 11:23:15 +0600

    type conversion revised

---
+++ typeConversion.py
@@ -0,0 +1,16 @@
+#birthYear = input("Enter your birth year? ")
+
+#print(type(birthYear))
+#age = 2024 - int(birthYear)
+
+#print(type(age))
+
+
+#print(age)
+
+
+weight_lbs = input("Give your weught in lbs")
+
+weight_Kg = int(weight_lbs) * .45
+
+print(weight_Kg)
