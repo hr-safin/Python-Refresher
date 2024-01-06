126a1bfcacd8516ac80a02ff5856c7990bfd396f
safinhr <hrsafin2434@gmail.com>
2024-01-06 11:41:05 +0600

    string revision

---
+++ string.py
@@ -0,0 +1,25 @@
+#multiline string
+
+#myString = '''
+
+#Dear Hiring Manager,
+
+#Hope this email finds you well
+
+#Thank you for your response
+
+#Sincerly
+#Hasibur Rahman Safin
+
+#'''
+
+#print(myString)
+
+
+name = "Python is my second love"
+
+print(name[0:])
+print(name[0:4])
+print(name[:2])
+print(name[:])
+print(name[1:-1])
