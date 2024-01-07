ecc6332c749e0199272775469c5d95511f6a0f72
safinhr <hrsafin2434@gmail.com>
2024-01-07 15:28:46 +0600

    comparsion operator basic problem

---
+++ comparisonOperator.py
@@ -0,0 +1,12 @@
+#
+
+name = "Hasibur rahman safin fdsjklllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllls"
+
+
+if(len(name) < 3):
+    print("Name must be 3 character")
+
+elif(len(name) > 50):
+    print("Name can be 50 character maximum")
+else:
+    print("name looks good")
