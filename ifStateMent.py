de12e214216777a1e1da01a1e01c8dd1095370cd
safinhr <hrsafin2434@gmail.com>
2024-01-07 13:34:56 +0600

    its a lovely day

---
+++ ifStateMent.py
@@ -0,0 +1,12 @@
+is_hot = False
+is_cold = False
+
+if (is_hot):
+    print("Its a hot day")
+    print("Drink plenty of water")
+elif (is_cold):
+    print("Its a cold day")
+    print("Wear worm cloth")
+
+else:
+    print("It is fall enjoy your day", "Lovely day")
