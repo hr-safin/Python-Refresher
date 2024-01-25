d1fabde9bb280a050fa25dc6f8937361eaeebdf9
safinhr <hrsafin2434@gmail.com>
2024-01-25 22:20:15 +0600

    arithmetic operation

--- arithmeticOperator.py
+++ arithmeticOperator.py
@@ -1,22 +1,20 @@
 
-
-x = 10
-
-x += 3
-
-print(x)
-
-y = 10
-
-y -= 7
-
-print(y)
-
 #exponentital
 #mulitplication 0r division
 #addition or substraction
 
 
-a = (4+5) * 5 - 7
+x = 45
+y = 65
 
-print(a)
+z = (x+y) + 20
+print(z)
+
+math = (8 + 7) * 5 - 7
+math2 = (5 + 6) * 6 - 7
+math3 = (4+6) * 56 -34
+print(math)
+print(math2)
+print(math3)
+
+
