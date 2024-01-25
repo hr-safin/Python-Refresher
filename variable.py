b0ca624ced9052b47c98d80429bd18f9e67492c2
safinhr <hrsafin2434@gmail.com>
2024-01-25 18:27:00 +0600

    variable : string, integer, boolean, float

--- variable.py
+++ variable.py
@@ -21,3 +21,26 @@ c = "for higher studies"
 
 print(a + b + c)
 
+
+# unpack collection
+
+number = ["safin", "hasibur", "hello"]
+
+a,b,c = number
+
+print(a)
+print(b)
+print(c)
+
+
+isAddict = True
+isNoAddict = False
+
+print(isAddict)
+print(isNoAddict)
+
+# python code is executed line by line
+m = "hello world"
+m = "Hasibur rahman safin"
+
+print(m)
