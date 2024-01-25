f1ba5e9fdf33ef5dc0ab742d89ba41f227b68059
safinhr <hrsafin2434@gmail.com>
2024-01-25 22:14:45 +0600

    string method

--- stringMethods.py
+++ stringMethods.py
@@ -1,2 +1,17 @@
 #string method
+# string is immutable we cannot change directly any strings
 
+name = '  hasibur rahman safin  '
+name2 = 'HASIBUR RAHMAN SAFIN'
+
+print(name.upper())
+print(name.lower())
+print(name.strip())
+name3 = name.split()
+print(name3)  #split into word and create array
+
+name = " ".join(name3)
+print(name)
+
+print(name.find("h"))
+print(name.replace("hasibur", "mahi"))
