16f327430dedccd286cb05ea24eb01b3b63cbb34
safinhr <hrsafin2434@gmail.com>
2024-01-25 23:02:11 +0600

    math function in python

--- mathFunciton.py
+++ mathFunciton.py
@@ -1,8 +1,21 @@
-import  math
+import math
 
-x = 2.7
+result = math.sqrt(25)
+print(result)
+result2 = math.pow(2,3)
+print(result2)
 
-print(round(x))
+result3 = math.ceil(3.1)
 
-print(math.floor(4.5))
-print(math.ceil(4.4))
+print(result3)
+
+result4 = math.floor(3.6)
+print(result4)
+
+# for absolute value we use fabs
+
+result5 = math.fabs(-5)
+print(result5)
+
+result6 = math.factorial(5)
+print(result6)
