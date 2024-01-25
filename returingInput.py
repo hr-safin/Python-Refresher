c37c1a7428e0d125517d4c836bc7b1f186d60620
safinhr <hrsafin2434@gmail.com>
2024-01-25 18:42:56 +0600

    gettingInput

--- returingInput.py
+++ returingInput.py
@@ -1,10 +1,17 @@
 #recieving input using input() function
-#printing output using output() function
+#printing output using print() function
 
 
-name = input("What is your name? ")
-age = input("What is your age? ")
-dream = input("What is your dream study country? ")
+# name = input("What is your name? ")
+# age = input("What is your age? ")
+# dream = input("What is your dream study country? ")
 
 
-print(name + " " +  age + " " + dream)
+# print(name + " " +  age + " " + dream)
+
+# doubled number
+
+num1 = int(input("Enter your number  :"))
+doubleNumber = 2 * num1
+
+print(doubleNumber)
