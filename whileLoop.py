659a35302966ad8cc3cf229ce49ac35cf63ddaeb
safinhr <hrsafin2434@gmail.com>
2024-01-26 17:36:59 +0600

    while loop

---
+++ whileLoop.py
@@ -0,0 +1,27 @@
+# i = 10
+
+# while i < 20:
+    # print(i)
+    # if(i == 15):
+        # break
+    # i += 1
+
+# print("done")
+
+
+j = 1
+
+while j < 7:
+    j +=1
+    if(j == 5):
+        continue
+    print(j)
+
+
+m = 1
+
+while m < 7:
+    print(m)
+    m+=1
+else:
+    print("m is no longer available")
