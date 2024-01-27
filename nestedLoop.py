30bf34f974f9a1a2028efcf3729ec2c2d5df8012
safinhr <hrsafin2434@gmail.com>
2024-01-27 22:51:16 +0600

    nested loop problem 1

---
+++ nestedLoop.py
@@ -0,0 +1,16 @@
+
+
+# for x in range(5):
+    # for y in range(3):
+       # print(f"({x},{y})")
+
+
+numbers = [5,2,5,2,2]
+
+for x_count in numbers:
+
+    output = ""
+    for count in range(x_count):
+        output += "X"
+    print(output)
+
