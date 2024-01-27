5e5782ef890b4bbfba6eb0f26ca2edb9a5a15163
safinhr <hrsafin2434@gmail.com>
2024-01-27 20:00:38 +0600

    car game using while loop

---
+++ carGame.py
@@ -0,0 +1,27 @@
+command = ""
+started = False
+while  True:
+    command = input("> ").lower()
+    if(command == "start"):
+        if(started):
+            print("Car is already started")
+        else:
+            started = True
+            print("Car starting")
+
+    elif(command == "stop"):
+        if(not started):
+            print('Car is already stopped')
+        else:
+            stopped = False
+            print("Car stopped")
+    elif(command == "help"):
+        print('''
+start -- to start the game
+stop -- to stop the game
+quit -- to quite the game
+        ''')
+    elif(command == "quit"):
+        break
+    else:
+        print("Sorry, I don't understand")
