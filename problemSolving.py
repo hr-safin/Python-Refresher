4a6fab62becb69b4fe91bb456f77ddb5e755c868
safinhr <hrsafin2434@gmail.com>
2024-01-27 23:02:01 +0600

    110: problem solved 1

---
+++ problemSolving.py
@@ -0,0 +1,8 @@
+# 1. Write a Python program to calculate the area of a rectangle given its length and width.
+
+length = 10
+width = 5
+
+rectangle = length * width
+
+print(f"The Area of Rectangle is {rectangle}")
