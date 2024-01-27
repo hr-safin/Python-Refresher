15ecfc1b7ec2744c96d2b62f0a3248e7fd333c7f
safinhr <hrsafin2434@gmail.com>
2024-01-27 22:54:25 +0600

    nested loop problem 2

--- nestedLoop.py
+++ nestedLoop.py
@@ -4,13 +4,23 @@
     # for y in range(3):
        # print(f"({x},{y})")
 
-
+# 1
 numbers = [5,2,5,2,2]
 
 for x_count in numbers:
 
     output = ""
     for count in range(x_count):
+        output += "X"
+    print(output)
+
+# 2
+
+numbers2 = [2,2,2,2,8]
+
+for l_count in numbers2:
+    output = ""
+    for count in range(l_count):
         output += "X"
     print(output)
 
